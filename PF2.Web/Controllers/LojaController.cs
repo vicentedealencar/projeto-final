360fb10a1bae704c5a1743c143d863f19d1c6fbd
Vicente de Alencar <vicentedealencar@gmail.com>
2013-07-23 12:48:48 -0300

    adding LojaController

---
+++ PF2.Web/Controllers/LojaController.cs
@@ -0,0 +1,18 @@
+﻿using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PF2.Web.Controllers
+{
+    public class LojaController : Controller
+    {
+        [GET("/Lojas")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
