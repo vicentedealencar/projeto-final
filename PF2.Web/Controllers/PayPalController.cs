a8ad13dd1a3ffc7eda1c5035ca4a3097aa6f8903
Vicente de Alencar <vicentedealencar@gmail.com>
2013-07-31 15:34:35 -0300

    (wip) paypal button testing sandbox and return method

---
+++ PF2.Web/Controllers/PayPalController.cs
@@ -0,0 +1,24 @@
+﻿using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PF2.Web.Controllers
+{
+    public class PayPalController : BaseController
+    {
+        [POST("/PayPal/Success")]
+        public ActionResult Success()
+        {
+            return Json(new{Form = Request.Form}, JsonRequestBehavior.AllowGet);
+        }
+
+        [GET("/PayPal/Cancel")]
+        public ActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
